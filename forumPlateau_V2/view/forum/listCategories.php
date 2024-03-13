50b9a7d24afc5b52db68bdc6347b0589f053d9e3
Alex Gthr <agauthier5@outlook.fr>
2024-03-13 16:24:11 +0100

    first commit

---
+++ forumPlateau_V2/view/forum/listCategories.php
@@ -0,0 +1,20 @@
+<?php
+    $categories = $result["data"]['categories']; 
+?>
+
+<h1>Liste des catégories</h1>
+
+<?php
+foreach($categories as $category ){?>
+
+    <div class="img_category">
+        <figure>
+            <img src="<?= $category->getPicture() ?>" title="<?= $category->getName() ?>">
+    </figure>
+    
+        <p><a href="index.php?ctrl=forum&action=listTopicsByCategory&id=<?= $category->getId() ?>"><?= $category->getName() ?></a></p>
+    </div>
+<?php }
+
+
+  
